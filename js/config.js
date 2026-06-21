3ae854eee6c5a341055930461da8243c53c31976
Sanan <asannan822@gmail.com>
2026-06-22 00:43:29 +0530

    Add Supabase configuration to config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.__SLM_CONFIG = {
+  supabaseUrl: 'https://tfvxxgtlygqzvethnopx.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmdnh4Z3RseWdxenZldGhub3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTk3MjYsImV4cCI6MjA5NzU5NTcyNn0.uSRickzZpJCqRLpjff3YBnJUgspVq6vOZYFR4qdr_-8'
+};
