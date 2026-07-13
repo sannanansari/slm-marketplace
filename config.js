50f4c8edf49c8684ee1d01f17fc2a97da3e317c4
sannanansari <asannan822@gmail.com>
2026-07-13 11:46:15 +0530

    update the config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__SLM_CONFIG = {
-  supabaseUrl: 'https://tfvxxgtlygqzvethnopx.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmdnh4Z3RseWdxenZldGhub3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTk3MjYsImV4cCI6MjA5NzU5NTcyNn0.uSRickzZpJCqRLpjff3YBnJUgspVq6vOZYFR4qdr_-8',
-    siteUrl: 'https://slm-marketplace.sannan.app'
+  url:     'https://tfvxxgtlygqzvethnopx.supabase.co',
+  key:     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmdnh4Z3RseWdxenZldGhub3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDJpYXQiOjE3NDJpYXQiOjE3NDJ....',
+  siteUrl: 'https://slm-marketplace.sannan.app'
 };
